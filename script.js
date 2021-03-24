9e6477c25759ba509145e51f04cb2a0b1172a8b7
Emily LeBlanc <emily.leblanc88@gmail.com>
2021-03-24 12:52:30 -0400

    pop-up navigation menu

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// Display navigation on hover over hamburger menu
+
+const exitIcon = document.getElementsByClassName('fa-times')
+
+const menuIcon = document.getElementsByClassName('fa-bars')
+
+const navigationMenu = document.querySelector('div')
+
+menuIcon[0].addEventListener('click', () => {
+    console.log('it clicked')
+    navigationMenu.classList.add('toggle-class')
+})
+
+exitIcon[0].addEventListener('click', () => {
+    console.log('clicky')
+    navigationMenu.classList.remove('toggle-class')
+})
+
+
+
+
+
