e8f8ec3801dfaa4c3292e9825654966a125bcb9c
Emily LeBlanc <emily.leblanc88@gmail.com>
2021-03-25 17:22:23 -0400

    change image sizes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-// Display navigation on hover over hamburger menu
+// Display navigation on click of hamburger menu
 
 const exitIcon = document.getElementsByClassName('fa-times')
 
@@ -17,6 +17,36 @@ exitIcon[0].addEventListener('click', () => {
     console.log('clicky')
     navigationMenu.classList.remove('toggle-class')
 })
+
+
+// setting up routing
+
+// application div
+// const appDiv = 'app';
+
+// // set of different routes and template generation functions
+
+// let routes = {};
+// let templates = {};
+
+// // register a template (this is to mimic a template engine)
+// let template = (name, templateFunction) => {
+//     return templates[name] = templateFunction;
+// };
+
+
+// // define the routes Each route is described with a route path
+
+// let route = (path, template) =>{
+//     if (typeof template == "function"){
+//         return routes[path] = template;
+//     }else if (typeof template == "srting"){
+//         return routes[path] = templates[template];
+//     }
+//     else{
+//         return;
+//     }
+// }
 
 
 
