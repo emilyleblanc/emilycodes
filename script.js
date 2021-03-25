44eb1182b357c2d1aa01a6d49931074b4df87fd9
Emily LeBlanc <emily.leblanc88@gmail.com>
2021-03-25 19:12:35 -0400

    added a success page

--- script.js
+++ script.js
@@ -19,34 +19,7 @@ exitIcon[0].addEventListener('click', () => {
 })
 
 
-// setting up routing
 
-// application div
-// const appDiv = 'app';
-
-// // set of different routes and template generation functions
-
-// let routes = {};
-// let templates = {};
-
-// // register a template (this is to mimic a template engine)
-// let template = (name, templateFunction) => {
-//     return templates[name] = templateFunction;
-// };
-
-
-// // define the routes Each route is described with a route path
-
-// let route = (path, template) =>{
-//     if (typeof template == "function"){
-//         return routes[path] = template;
-//     }else if (typeof template == "srting"){
-//         return routes[path] = templates[template];
-//     }
-//     else{
-//         return;
-//     }
-// }
 
 
 
